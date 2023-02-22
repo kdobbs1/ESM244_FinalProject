ae33abb5b9b2e62617627dff310a6a3816d1a366
kdobbs1 <kdobbs@bren.ucsb.edu>
2023-02-22 11:12:13 -0800

    tab creation

--- app.R/app.R
+++ app.R/app.R
@@ -1,7 +1,13 @@
 library(shiny)
 library(tidyverse)
 
-ui <- fluidPage()
+ui <- fluidPage(
+  navbarPage("My app name",
+             tabPanel("Tab 1"),
+             tabPanel("Tab 2"),
+             tabPanel("Tab 3")
+             )
+)
 
 server <- function(input, output) {}
 
